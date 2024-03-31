6b12a3f238442dc0ca45218c0320e27e73631c00
kanaru-ssk <kanaru.ssk@gmail.com>
2024-03-31 14:40:04 +0000

    create footer component

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,3 +1,11 @@
+import { Typography } from "@mui/material";
+
 export function Footer() {
-  return <footer>footer</footer>;
+  return (
+    <footer className="absolute bottom-0 w-full bg-black p-10 text-center">
+      <Typography variant="body1" component="p" sx={{ color: "white" }}>
+        footer
+      </Typography>
+    </footer>
+  );
 }
